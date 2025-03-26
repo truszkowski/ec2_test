0a047fdc2e563e8f9eb0cf16a66c5d8e36f6d292
Piotr Truszkowski <truszkowski@gmail.com>
2025-03-26 13:25:49 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "amazon2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] # Amazon
+}
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.amazon2.id
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "Test"
+  }
+}

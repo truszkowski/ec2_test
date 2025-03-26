010772155e851c38b67be8eb43ffa667d7f97e8c
Piotr Truszkowski <truszkowski@gmail.com>
2025-03-26 13:38:41 +0100

    add outputs and variables

--- main.tf
+++ main.tf
@@ -34,11 +34,27 @@ data "aws_ami" "amazon2" {
   owners = ["137112412989"] # Amazon
 }
 
+variable instance_type {
+  default = "t2.micro"
+}
+
 resource "aws_instance" "test" {
   ami           = data.aws_ami.amazon2.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type.id
 
   tags = {
     Name = "Test"
   }
+}
+
+output public_ip {
+  value = aws_instance.test.public_ip
+}
+
+output public_dns {
+  value = aws_instance.test.public_dns
+}
+
+output instance_type {
+  value = aws_instance.test.instance_type
 }
